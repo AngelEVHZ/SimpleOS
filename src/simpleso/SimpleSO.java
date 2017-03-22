300b05d684363f91f8e2c08fdeba38b92669a80c
Vaio <Vaio@Vaio-PC.mshome.net>
2017-03-22 06:25:57 -0600

    Nueva actualización, se implemente el escritorio (chafa :'v)

--- src/simpleso/SimpleSO.java
+++ src/simpleso/SimpleSO.java
@@ -16,13 +16,33 @@ import javafx.stage.Stage;
  * @author Valenzuela
  */
 public class SimpleSO extends Application {
-    
+    static Stage stage;
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));     
+        this.stage = stage;
+        
+        //Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("FXMLBrowser.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("escritorio/FXMLescritorio.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+    public void escritorio() throws Exception{
+        this.stage.close();
+        Parent root = FXMLLoader.load(getClass().getResource("escritorio/FXMLescritorio.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = this.stage;
+        this.stage = stage;
+        stage.setScene(scene);
+        stage.setTitle("SimpleSO");
+        stage.setFullScreen(true);
+        stage.show();
+    }
+    public void abrir_ventana(Parent root){
+        Stage stage = new Stage();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
     }
 
     /**
